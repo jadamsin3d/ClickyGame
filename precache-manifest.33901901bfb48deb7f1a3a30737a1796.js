efebe0c200a4916a81c22ee9080e83810c67cdb3
jadamsin3d <jadamsin3d@gmail.com>
2019-03-06 18:31:07 -0800

    Updates

---
+++ precache-manifest.33901901bfb48deb7f1a3a30737a1796.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c3a1b5a67c5d677a07d",
+    "url": "/ClickyGame/static/js/runtime~main.2c3a1b5a.js"
+  },
+  {
+    "revision": "c33c0a337687fc6dafa7",
+    "url": "/ClickyGame/static/js/main.c33c0a33.chunk.js"
+  },
+  {
+    "revision": "7b587c9f73424b6624eb",
+    "url": "/ClickyGame/static/js/2.7b587c9f.chunk.js"
+  },
+  {
+    "revision": "c33c0a337687fc6dafa7",
+    "url": "/ClickyGame/static/css/main.6a4cf339.chunk.css"
+  },
+  {
+    "revision": "61a483eb69b924ed9a3da77956efd999",
+    "url": "/ClickyGame/index.html"
+  }
+];
