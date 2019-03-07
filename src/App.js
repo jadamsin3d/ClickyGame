5f8aadf37e53022e5d8a3ff8a70f52a4c2b81317
jadamsin3d <jadamsin3d@gmail.com>
2019-03-06 19:13:02 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Header from "./components/Header";
-import NavBar from "./components/NavBar";
+// import Header from "./components/Header";
+// import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import "./App.css";
 
